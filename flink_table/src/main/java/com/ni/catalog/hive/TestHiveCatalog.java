43deb33570d906beb9f83cf9cdef691bc3317552
倪梓豪 <nzh9900@163.com>
2022-05-17 16:21:14 +0800

    feature: iceberg test

--- flink_table/src/main/java/com/ni/catalog/hive/TestHiveCatalog.java
+++ flink_table/src/main/java/com/ni/catalog/hive/TestHiveCatalog.java
@@ -11,7 +11,36 @@ public class TestHiveCatalog {
 
     public static void main(String[] args) throws Exception {
         StreamTableEnvironment tEnv = init();
-        scanTableWithHiveCatalog(tEnv);
+        setHiveCatalog(tEnv);
+        for (String arg : args) {
+            switch (arg) {
+                case "hive":
+                    getSomeHiveInformation(tEnv);
+                    break;
+                case "icebergInsert":
+                    insertIntoIcebergTable(tEnv);
+                    break;
+                case "createIcebergTable":
+                    insertIntoIcebergTable(tEnv);
+                    break;
+                case "icebergRead":
+                    break;
+            }
+        }
+    }
+
+    private static void createIcebergTable(StreamTableEnvironment tEnv) throws Exception {
+        tEnv.sqlQuery("create table if not exists ice_01 (id int,name string) " +
+                "with ('connector'='iceberg'," +
+                "'catalog-name'='hive_prod'," +
+                "'catalog-database'='datalake'," +
+                "'catalog-table'='iceberg_001')");
+    }
+
+    private static void insertIntoIcebergTable(StreamTableEnvironment tEnv) {
+        tEnv.executeSql("create  table if not exists source (id int,name string) with ('connector' = 'datagen')");
+        tEnv.executeSql("insert into sink_test2 select * from source");
+
     }
 
     private static StreamTableEnvironment init() {
@@ -20,29 +49,22 @@ public class TestHiveCatalog {
         return StreamTableEnvironment.create(env, EnvironmentSettings.newInstance().inStreamingMode().useBlinkPlanner().build());
     }
 
-    private static void scanTableWithHiveCatalog(StreamTableEnvironment tEnv) throws Exception {
+    private static void setHiveCatalog(StreamTableEnvironment tEnv) {
         HiveCatalog hiveCatalog =
-                new HiveCatalog("hiveCatalog", "datalake", "/home/ni", "3.1.2");
+                new HiveCatalog("hiveCatalog", "datalake",
+                        "/etc/hive/conf", "3.1.2");
         tEnv.registerCatalog("hiveCatalog", hiveCatalog);
         tEnv.useCatalog("hiveCatalog");
+        System.out.println("==========================using hive catalog============================");
+    }
+
+    private static void getSomeHiveInformation(StreamTableEnvironment tEnv) {
+        System.out.println("==========================list catalogs============================");
         Arrays.stream(tEnv.listCatalogs()).forEach(System.out::println);
-        System.out.println("======================================================");
+        System.out.println("==========================list databases============================");
         Arrays.stream(tEnv.listDatabases()).forEach(System.out::println);
-        System.out.println("======================================================");
+        System.out.println("==========================list tables============================");
         tEnv.useDatabase("datalake");
         Arrays.stream(tEnv.listTables()).forEach(System.out::println);
-        System.out.println("======================================================");
-        //tEnv.executeSql("DESCRIBE iceberg_001").print();
-        System.out.println("======================================================");
-        //tEnv.executeSql("select * from hiveCatalog.datalake.iceberg_001").print();
-        System.out.println("===========================start===========================");
-        //tEnv.executeSql("drop table source");
-        tEnv.executeSql("create  table if not exists source  (id int,name string) with ('connector' = 'datagen')");
-        tEnv.executeSql("insert into sink_test2 select * from source");
-
-        //tEnv.executeSql("insert into hiveCatalog.datalake.iceberg_001 select * from source");
-
-
-        //tEnv.execute("iceberg Test");
     }
 }
