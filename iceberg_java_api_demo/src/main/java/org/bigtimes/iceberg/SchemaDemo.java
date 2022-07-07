1230a74755890b949958404d20c80873d1323070
倪梓豪 <nzh9900@163.com>
2022-07-07 10:59:54 +0800

    feature: Mysql CDC and iceberg sink Test

---
+++ iceberg_java_api_demo/src/main/java/org/bigtimes/iceberg/SchemaDemo.java
@@ -0,0 +1,16 @@
+package org.bigtimes.iceberg;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.hive.HiveCatalog;
+
+public class SchemaDemo {
+    public static void main(String[] args) {
+        HiveCatalog hiveCatalog = new HiveCatalog();
+        //hiveCatalog.setConf(new Configuration());
+        //getSchema()
+    }
+
+    private static void getSchema() {
+
+    }
+}
