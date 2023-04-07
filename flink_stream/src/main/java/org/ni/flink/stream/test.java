4aeecde67dd4c975d28f428c47bc1e31e8fdbefe
倪梓豪 <nzh9900@163.com>
2023-04-07 17:49:56 +0800

    build: 重建项目

--- flink_stream/src/main/java/org/ni/flink/stream/test.java
+++
@@ -1,9 +0,0 @@
-package org.ni.flink.stream;
-
-public class test {
-    public static void main(String[] args) {
-
-    }
-}
-
-
--- flink_table/src/main/java/com/test/ServletTest.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public class ServletTest {
-    public static void main(String[] args) {
-
-    }
-}
