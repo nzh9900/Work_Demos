635533d9ff6a03937bc32e3f3ab951cda53fd6f6
倪梓豪 <nzh9900@163.com>
2023-05-05 10:08:55 +0800

    feature: exception打印测试

--- demo1/src/test/java/com/ni/test/Test01.java
+++ demo1/src/test/java/com/ni/test/Test01.java
@@ -1,6 +1,8 @@
 package com.ni.test;
 
 
+import org.junit.Test;
+
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -11,9 +13,20 @@ import java.io.UnsupportedEncodingException;
  * @Version 1.0
  **/
 public class Test01 {
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    @Test
+    public void testA() throws UnsupportedEncodingException {
         String a = "���������������������";
         String s = new String(a.getBytes(), "UTF-8");
         System.out.println(s);
+    }
+
+    @Test
+    public void testB() {
+        RuntimeException runtimeException = new RuntimeException();
+        System.out.println(runtimeException.getMessage());
+        System.out.println("====================");
+        runtimeException.printStackTrace();
+        System.out.println("====================");
+        System.out.println(runtimeException.getLocalizedMessage());
     }
 }
