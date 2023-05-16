65ea5e4f224680dd8415cc25edf43bb0765010df
nzh9900 <nzh9900.163.com>
2023-05-16 14:41:22 +0800

    feature: log module init

--- log-framework/src/main/java/org/example/Main.java
+++ log-framework/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("info---");
+        log.debug("debug---");
+        log.error("error---");
+        log.warn("warn---");
+        log.trace("trace---");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                log.info("sadasdasd");
+            }
+        };
+        Thread aa = new Thread(runnable,"AA");
+        aa.run();
+
     }
 }
