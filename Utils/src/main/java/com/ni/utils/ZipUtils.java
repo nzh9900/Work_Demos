e79055e8892a9c96e95562a9b42186992072624b
nzh9900 <nzh9900.163.com>
2023-06-21 14:31:13 +0800

    feature: ZipUtils.java

---
+++ Utils/src/main/java/com/ni/utils/ZipUtils.java
@@ -0,0 +1,29 @@
+package com.ni.utils;
+
+import java.io.File;
+import java.nio.file.Path;
+
+/**
+ * @ClassName ZipUtils
+ * @Description zip压缩工具
+ * @Author zihao.ni
+ * @Date 2023/6/21 10:08
+ * @Version 1.0
+ **/
+public class ZipUtils {
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 将输入的文件压缩成zip格式的压缩包
+     *
+     * @param file 被压缩的文件
+     * @param outputPath 压缩包保存路径
+     */
+    public static void toZip(File file, Path outputPath) {
+
+
+    }
+}
