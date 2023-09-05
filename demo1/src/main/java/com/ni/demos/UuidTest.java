5775ace200ff3b1e79487f5c323223a83df3a239
nzh9900 <nzh9900.163.com>
2023-09-05 18:30:32 +0800

    feature: uuid test

---
+++ demo1/src/main/java/com/ni/demos/UuidTest.java
@@ -0,0 +1,17 @@
+package com.ni.demos;
+
+import java.util.UUID;
+
+/**
+ * @ClassName UuidTest
+ * @Description
+ * @Author zihao.ni
+ * @Date 2023/9/5 18:28
+ * @Version 1.0
+ **/
+public class UuidTest {
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+    }
+}
