a4a1740371a678e6e6a1fdd3103ec4a664018760
nzh9900 <nzh9900.163.com>
2023-12-28 09:56:45 +0800

    feature: add hive metastore api usage

---
+++ hive_usage/src/main/java/com/ni/demo/HiveJdbc.java
@@ -0,0 +1,44 @@
+package com.ni.demo;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * @ClassName HiveJdbc
+ * @Description todo
+ * @Author zihao.ni
+ * @Date 2023/12/27 14:18
+ * @Version 1.0
+ **/
+public class HiveJdbc {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.apache.hive.jdbc.HiveDriver");
+        //Configuration conf = new Configuration();
+        //String principal = "idp";
+        //String keytabFile = "/opt/idp.keytab";
+        //conf.set("hadoop.security.authentication", "kerberos");
+        //conf.set("hadoop.security.authorization", "true");
+        //UserGroupInformation.setConfiguration(conf);
+        //try {
+        //    UserGroupInformation.loginUserFromKeytab(principal, keytabFile);
+        //    System.out.println("认证方式和用户: " + UserGroupInformation.getLoginUser());
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
+
+        try {
+            java.sql.Connection connection = java.sql.DriverManager.getConnection("jdbc:hive2://cherry.lab.com:10000/default;principal=hive/cherry.lab.com@RUISDATA.COM");
+            java.sql.Statement statement = connection.createStatement();
+            String sql = "show databases";
+            java.sql.ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- hive_usage/src/main/java/com/ni/demo/HiveMetastoreUsage.java
+++ hive_usage/src/main/java/com/ni/demo/HiveMetastoreUsage.java
@@ -4,11 +4,15 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
-import org.apache.hadoop.hive.metastore.api.MetaException;
+import org.apache.hadoop.hive.metastore.TableType;
+import org.apache.hadoop.hive.metastore.api.*;
 import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.thrift.TException;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @ClassName HiveMetastoreUsage
@@ -20,7 +24,7 @@ import java.util.Arrays;
 public class HiveMetastoreUsage {
     public HiveMetaStoreClient hiveMetaStoreClient;
 
-    public static void main(String[] args) throws MetaException {
+    public static void main(String[] args) throws TException {
         HiveMetastoreUsage hiveMetastoreUsage = new HiveMetastoreUsage();
 
 
@@ -38,14 +42,101 @@ public class HiveMetastoreUsage {
         }
         hiveMetastoreUsage.getConnection();
 
-        System.out.println(hiveMetastoreUsage.hiveMetaStoreClient.getAllDatabases());
-        hiveMetastoreUsage.hiveMetaStoreClient.getTableMeta("","", Arrays.asList(""));
+        List<FieldSchema> partitionKeys = hiveMetastoreUsage.getPartitionKeys("default", "test_888");
+        System.out.println(partitionKeys.size());
+        partitionKeys.forEach(fieldSchema -> System.out.println(fieldSchema.getName()));
 
+        List<FieldSchema> fields = hiveMetastoreUsage.getFields("default", "test_888");
+        hiveMetastoreUsage.printFieldDetail(fields.get(0));
     }
 
+    /**
+     * 获取连接
+     *
+     * @throws MetaException
+     */
     private void getConnection() throws MetaException {
         HiveConf conf = new HiveConf();
-        conf.addResource(new Path("hive-site.xml"));
+        conf.addResource(new Path("/Users/ni/IdeaProjects/Work_Demos/hive_usage/src/main/resources/hive-site-cdh6.xml"));
+        //conf.set("hive.metastore.uris", "thrift://node22.test.com:9083,thrift://node23.test.com:9083");
+        System.out.println(System.currentTimeMillis());
         hiveMetaStoreClient = new HiveMetaStoreClient(conf);
+        System.out.println(System.currentTimeMillis());
+    }
+
+    /**
+     * 建表
+     *
+     * @throws TException
+     */
+    private void createTable() throws TException {
+        Table hiveTable = new Table();
+        hiveTable.setDbName("default");
+        hiveTable.setTableName("test_911");
+        hiveTable.setTableType(TableType.MANAGED_TABLE.name());
+        List<FieldSchema> PARTITION_COLUMNS = Arrays.asList(new FieldSchema("continent", "string", ""),
+                new FieldSchema("country", "string", "a\nlntry\t918"));
+        List<FieldSchema> DATA_COLUMNS = Arrays.asList(new FieldSchema("id", "bigint", ""),
+                new FieldSchema("name", "string", ""), new FieldSchema("city", "tinyint", ""));
+        hiveTable.setPartitionKeys(PARTITION_COLUMNS);
+
+        StorageDescriptor sd = new StorageDescriptor();
+        sd.setCols(DATA_COLUMNS);
+        sd.setParameters(new HashMap<String, String>());
+        sd.setSerdeInfo(new SerDeInfo());
+
+        hiveTable.setSd(sd);
+
+        hiveMetaStoreClient.createTable(hiveTable);
+    }
+
+    /**
+     * 获取所有表名
+     *
+     * @return
+     * @throws MetaException
+     */
+    private List<String> getAllTableNames(String dbName) throws MetaException {
+        return hiveMetaStoreClient.getAllTables(dbName);
+    }
+
+    /**
+     * 获取表字段
+     *
+     * @param dbName
+     * @param tableName
+     * @return
+     * @throws TException
+     */
+    private List<FieldSchema> getFields(String dbName, String tableName) throws TException {
+        return hiveMetaStoreClient.getFields(dbName, tableName);
+    }
+
+    /**
+     * 获取字段信息
+     *
+     * @param field
+     */
+    private void printFieldDetail(FieldSchema field) {
+        String comment = field.getComment();
+        System.out.println(comment);
+        System.out.println();
+        for (int i = 0; i < comment.length(); i++) {
+            char c = comment.charAt(i);
+            System.out.println(i + ":" + ('\n' == c) + ":" + c);
+        }
+    }
+
+    /**
+     * 获取分区字段
+     *
+     * @param dbName
+     * @param tableName
+     * @return
+     * @throws TException
+     */
+    private List<FieldSchema> getPartitionKeys(String dbName, String tableName) throws TException {
+        Table table = hiveMetaStoreClient.getTable(dbName, tableName);
+        return table.getPartitionKeys();
     }
 }
