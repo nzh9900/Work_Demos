8e28cf99d06a3517451a3aa5c4d5d6645251a821
nzh9900 <nzh9900.163.com>
2024-02-27 15:37:32 +0800

    feature: 通过Flink metric监控类型为source和sink的flink sql的输入输出数据条数

--- Flink-SQL/src/main/java/com/ni/flink/FlinkSqlMetric.java
+++ Flink-SQL/src/main/java/com/ni/flink/FlinkSqlMetric.java
@@ -3,19 +3,63 @@ package com.ni.flink;
 import com.ni.flink.function.CountMessageFunction;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.types.DataType;
 
 /**
  * 通过Flink metric监控类型为source和sink的flink sql的输入输出数据条数
  */
 public class FlinkSqlMetric {
     public static void main(String[] args) throws Exception {
+        //FlinkSqlMetric.kafkaTest(new String[]{""});
+        FlinkSqlMetric.cdcTest();
+
+    }
+
+
+    public static void cdcTest() {
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+        tEnv.executeSql("CREATE TABLE IF NOT EXISTS `source_table` (\n" +
+                "\t`id`\tVARCHAR(200)\n" +
+                ",\t`name`\tVARCHAR(20)\n" +
+                ",\t`school`\tVARCHAR(20)\n" +
+                ",\t`nickname`\tVARCHAR(20)\n" +
+                ",\t`age`\tINTEGER\n" +
+                ",\t`class_num`\tINTEGER\n" +
+                ",\t`score`\tDECIMAL(4,2)\n" +
+                ",\t`phone`\tBIGINT\n" +
+                ",\t`email`\tVARCHAR(64)\n" +
+                ",\t`ip`\tVARCHAR(32)\n" +
+                ",\t`address`\tSTRING\n" +
+                ",\t`proctime` AS PROCTIME ()\n" +
+                ",\tPRIMARY KEY (`id`) NOT ENFORCED\n" +
+                ") WITH (\n" +
+                "\t'connector' = 'mysql-cdc',\n" +
+                "\t'hostname' = '10.24.96.222',\n" +
+                "\t'port' = '3307',\n" +
+                "\t'username' = 'root',\n" +
+                "\t'password' = '123456',\n" +
+                "\t'database-name' = 'demo',\n" +
+                "\t'table-name' = 'stu_for_source',\n" +
+                "\t'scan.startup.mode' = 'latest-offset'\n" +
+                ")");
+
+        DataType dataType = tEnv.from("source_table").getResolvedSchema().getColumn(0).get().getDataType();
+
+        tEnv.createTemporaryFunction("count_message",
+                new CountMessageFunction("MyGroup", "mysqlcdc", dataType));
+
+        tEnv.executeSql("select count_message(id) as countA from source_table").print();
+    }
+
+    public static void kafkaTest(String[] args) throws Exception {
         StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
         tEnv.executeSql("CREATE TABLE fact_table (\n" +
-                "  `id` BIGINT,\n" +
+                "  `id` STRING,\n" +
                 "  `ts` TIMESTAMP(3) METADATA FROM 'timestamp',\n" +
                 "  `process_time` as proctime()\n" +
                 ") WITH (\n" +
@@ -27,15 +71,25 @@ public class FlinkSqlMetric {
                 "  'format' = 'csv'\n" +
                 ")");
 
-        tEnv.createTemporaryFunction("count_message", new CountMessageFunction("fact_table_1"));
+        tEnv.executeSql("CREATE TABLE sink_table (\n" +
+                "  `id` STRING,\n" +
+                "  `ts` TIMESTAMP(3),\n" +
+                "  `process_time` as proctime()\n" +
+                ") WITH (\n" +
+                "  'connector' = 'kafka',\n" +
+                "  'topic' = 'test',\n" +
+                "  'properties.bootstrap.servers' = '10.24.68.224:9092',\n" +
+                "  'properties.group.id' = 'testGroup1',\n" +
+                "  'scan.startup.mode' = 'latest-offset',\n" +
+                "  'format' = 'csv'\n" +
+                ")");
 
-        TableResult tableResult = tEnv.executeSql("select count_message(0) from fact_table");
-        tableResult.print();
+        DataType dataType = tEnv.from("sink_table").getResolvedSchema().getColumn(0).get().getDataType();
 
-        tEnv.executeSql("CREATE TABLE blackhole_table WITH ('connector' = 'blackhole')\n" +
-                "LIKE fact_table (EXCLUDING ALL)");
+        tEnv.createTemporaryFunction("count_message", new CountMessageFunction("MyGroup", "sourceKafka", dataType));
+        tEnv.createTemporaryFunction("count_sink", new CountMessageFunction("MyGroup", "sinkKafka", dataType));
 
-        tEnv.executeSql("INSERT INTO blackhole_table SELECT id FROM fact_table");
+        tEnv.executeSql("insert into sink_table select id,ts as ts from (select count_sink(id) as id,ts as ts from fact_table)");
 
         tEnv.execute("metric test");
     }
--- Flink-SQL/src/main/java/com/ni/flink/function/CountMessageFunction.java
+++ Flink-SQL/src/main/java/com/ni/flink/function/CountMessageFunction.java
@@ -2,10 +2,17 @@ package com.ni.flink.function;
 
 import org.apache.flink.metrics.Counter;
 import org.apache.flink.metrics.MetricGroup;
-import org.apache.flink.table.annotation.DataTypeHint;
-import org.apache.flink.table.annotation.InputGroup;
+import org.apache.flink.table.catalog.DataTypeFactory;
 import org.apache.flink.table.functions.FunctionContext;
+import org.apache.flink.table.functions.FunctionDefinition;
 import org.apache.flink.table.functions.ScalarFunction;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.inference.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @ClassName CountMessageFunction
@@ -18,20 +25,149 @@ public class CountMessageFunction extends ScalarFunction {
     private transient Counter counter;
 
     private final String counterName;
+    private final String groupName;
+    private final DataType dataType;
 
-    public CountMessageFunction(String counterName) {
+
+    public CountMessageFunction(String groupName, String counterName, DataType dataType) {
+        this.groupName = groupName;
         this.counterName = counterName;
+        this.dataType = dataType;
     }
 
     @Override
     public void open(FunctionContext context) throws Exception {
-        MetricGroup metricGroup = context.getMetricGroup();
+        MetricGroup metricGroup = context.getMetricGroup().addGroup(groupName);
         this.counter = metricGroup.counter(counterName);
     }
 
-    public String eval(@DataTypeHint(inputGroup = InputGroup.ANY) Object o) {
+    //@DataTypeHint(version = ExtractionVersion.V1, value = "TIMESTAMP(3)")
+    //public Object eval(@DataTypeHint(inputGroup = InputGroup.ANY) Object o) {
+    //    counter.inc();
+    //    return o;
+    //}
+
+    public Object eval(Object o) {
         counter.inc();
-        System.out.println("count: " + counter.getCount());
-        return o.toString();
+        return o;
     }
+
+    @Override
+    public TypeInference getTypeInference(DataTypeFactory typeFactory) {
+        return TypeInference.newBuilder()
+                .inputTypeStrategy(new MyInputTypeStrategy())
+                .outputTypeStrategy(new MyOutputTypeStrategy())
+                .build();
+    }
+
+    private class MyInputTypeStrategy implements InputTypeStrategy {
+        private MyInputTypeStrategy() {
+
+        }
+
+        @Override
+        public ArgumentCount getArgumentCount() {
+            return ConstantArgumentCount.any();
+        }
+
+        @Override
+        public Optional<List<DataType>> inferInputTypes(CallContext callContext, boolean throwOnFailure) {
+            return Optional.of(Arrays.asList(dataType));
+        }
+
+        @Override
+        public List<Signature> getExpectedSignatures(FunctionDefinition definition) {
+            return Collections.singletonList(Signature.of(new Signature.Argument[]{Signature.Argument.of("*")}));
+        }
+    }
+
+    private class MyOutputTypeStrategy implements TypeStrategy {
+        private MyOutputTypeStrategy() {
+        }
+
+        @Override
+        public Optional<DataType> inferType(CallContext callContext) {
+            return Optional.of(dataType);
+        }
+    }
+
+    /*public String eval(String value) {
+        counter.inc();
+        return value;
+    }
+
+    public Integer eval(Integer value) {
+        counter.inc();
+        return value;
+    }
+
+    public Double eval(Double value) {
+        counter.inc();
+        return value;
+    }
+
+    public Long eval(Long value) {
+        counter.inc();
+        return value;
+    }
+
+    public Boolean eval(Boolean value) {
+        counter.inc();
+        return value;
+    }
+
+    public Byte eval(Byte value) {
+        counter.inc();
+        return value;
+    }
+
+    public Short eval(Short value) {
+        counter.inc();
+        return value;
+    }
+
+    public Float eval(Float value) {
+        counter.inc();
+        return value;
+    }
+
+    public LocalDate eval(LocalDate value) {
+        counter.inc();
+        return value;
+    }
+
+    public Date eval(Date value) {
+        counter.inc();
+        return value;
+    }
+
+    public Time eval(Time value) {
+        counter.inc();
+        return value;
+    }
+
+    public LocalTime eval(LocalTime value) {
+        counter.inc();
+        return value;
+    }
+
+    public LocalDateTime eval(LocalDateTime value) {
+        counter.inc();
+        return value;
+    }
+
+    public Timestamp eval(Timestamp value) {
+        counter.inc();
+        return value;
+    }
+
+    public OffsetDateTime eval(OffsetDateTime value) {
+        counter.inc();
+        return value;
+    }
+
+    public Duration eval(Duration value) {
+        counter.inc();
+        return value;
+    }*/
 }
