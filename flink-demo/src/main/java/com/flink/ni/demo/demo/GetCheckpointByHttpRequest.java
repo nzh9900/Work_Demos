ba26a5f809f12edf0e092526466e7e9e597447dc
nzh9900 <nzh9900.163.com>
2024-03-07 15:32:32 +0800

    feature: create checkpoint bean

--- flink-demo/src/main/java/com/flink/ni/demo/demo/GetCheckpointByHttpRequest.java
+++ flink-demo/src/main/java/com/flink/ni/demo/demo/GetCheckpointByHttpRequest.java
@@ -1,7 +1,16 @@
 package com.flink.ni.demo.demo;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flink.ni.demo.checkpoint.CheckPointOverView;
 import com.flink.ni.demo.common.Constants;
 import com.xiaoleilu.hutool.http.HttpUtil;
+
+import java.util.TimeZone;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.*;
+import static com.fasterxml.jackson.databind.MapperFeature.REQUIRE_SETTERS_FOR_GETTERS;
 
 
 /**
@@ -12,16 +21,27 @@ import com.xiaoleilu.hutool.http.HttpUtil;
  * @Version 1.0
  **/
 public class GetCheckpointByHttpRequest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         String host = "ni";
         int port = 8190;
-        String jobId = "1d0a0dcc6f1184510b47bbda13df384d";
+        String jobId = "20d9e6761cdff315752d4ed3be44c2d2";
 
         GetCheckpointByHttpRequest client = new GetCheckpointByHttpRequest();
         String clusterUrl = Constants.HTTP + client.getClusterUrl(host, port);
-        String checkpointInfo = client.getCheckpointInfo(clusterUrl, jobId);
 
-        System.out.println(checkpointInfo);
+        clusterUrl = "http://node24.test.com:8088/proxy/application_1695812289210_100531/";
+        String checkpointInfo = client.getCheckpointInfo(clusterUrl, jobId);
+        ObjectMapper objectMapper = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true)
+                .configure(READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+                .configure(REQUIRE_SETTERS_FOR_GETTERS, true)
+                .setTimeZone(TimeZone.getDefault());
+        ;
+        CheckPointOverView checkPointOverView = objectMapper.readValue(checkpointInfo, CheckPointOverView.class);
+        System.out.println("1." + JSON.toJSONString(checkPointOverView));
+        System.out.println("2." + checkPointOverView);
+
+        System.out.println("3." + checkpointInfo);
     }
 
     public String getClusterUrl(String host, int port) {
